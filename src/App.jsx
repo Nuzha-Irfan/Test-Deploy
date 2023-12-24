15eaa66b1b84eae853b1d491c10880c7cabdc9e5
NuzhaCreative-Hub <nuzha.creativehub@gmail.com>
2023-12-24 11:32:59 +0530

    intial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Routes from "./Routes";
+import ProjectRoutes from "./Routes";
 
 function App() {
-  return <Routes />;
+  return <ProjectRoutes />;
 }
 
 export default App;
